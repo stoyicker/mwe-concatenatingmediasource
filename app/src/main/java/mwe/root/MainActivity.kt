17cbefcab5de512bc67b431c97e6cf8f1d6ec3b9
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2023-09-06 11:46:28 +0200

    Setup template

---
+++ app/src/main/java/mwe/root/MainActivity.kt
@@ -0,0 +1,11 @@
+package mwe.root
+
+import android.app.Activity
+import android.os.Bundle
+
+internal class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
